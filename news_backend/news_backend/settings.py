f46fe264f5cba54521795e1f2486b28101b721f2
Dhruv Shounak <dhruvshounak@Dhruvs-MacBook-Air.local>
2025-06-13 14:34:22 +0530

    API key handling included

--- news_backend/news_backend/settings.py
+++ news_backend/news_backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c3pbaw1et*u-js=l@o5a8-(p2d97$!%8v&fi!_jcom=kg*@fl#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
